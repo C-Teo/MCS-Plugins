9f05f3ecfbfcceaf6aec8b514db93975d62b9a05
Teo <90730116+C-Teo@users.noreply.github.com>
2022-10-08 00:37:26 -0400

    Java

---
+++ Placeholder.java
@@ -0,0 +1,11 @@
+public class Placeholder {
+    public static void main(String[] args) {
+        /**
+         * @author Teo
+         * This is a placeholder Java file so the repository is 
+         * marked as a java repo. Even though there is no code
+         * in this repo, it is still purposed to explain the
+         * the components of java applications.
+         */
+    }
+}
